121ed6255d870d3c4273871c5723fa6f6703cdcc
Mehul Sonowal <mehulsonowal@gmail.com>
2019-03-30 15:03:19 -0700

    Accessing URLS

--- app.js
+++ app.js
@@ -1,135 +1,148 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const router = express.Router();
-const axios = require('axios');
-const cheerio = require('cheerio');
-console.log('here');
+const axios = require("axios");
+const cheerio = require("cheerio");
+console.log("here");
 
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-
-
-router.get('/data/:id', function(req,res){
-let myData = null
-const apiUrl = 'https://us-central1-vision-migration.cloudfunctions.net/la_hacks_2019?market_code=1'
-axios.get(apiUrl)
-  .then(response => {
-    myData = response.data
-    console.log(response.data)
+router.get("/data/:id", function(req, res) {
+  let myData = null;
+  const apiUrl =
+    "https://us-central1-vision-migration.cloudfunctions.net/la_hacks_2019?market_code=1";
+  axios.get(apiUrl).then(response => {
+    myData = response.data;
+    console.log(response.data);
     response.data.buckets[0].report.rollups.forEach(item => {
       console.log(item.name);
-    })
-  var array = [];
-  response.data.buckets[0].report.rollups[req.params.id].top_articles_on_network.forEach(item => {
-        if(array.indexOf(item) == -1) array.push(item);
     });
-  res.render('detail', {data: array});
-  console.log(myData)
-  })
-  .catch(error => {
-    console.log(error);
-  })
-//display the specific articles for a trending topic
+    var array = [];
+    response.data.buckets[0].report.rollups[
+      req.params.id
+    ].top_articles_on_network.forEach(item => {
+      if (array.indexOf(item) == -1) array.push(item);
+    });
 
-//display the result of google cloud analysis of this topic.
+    response.data.buckets[0].report.rollups[
+      req.params.id
+    ].top_articles_on_network
+      .forEach(item => {
+        const url = Object.keys(item);
+        axios
+          .get(url[0])
+          .then(response => {
+            //console.log(response.data);
+            console.log(cheerio("p", response.data).text());
+          })
+          .catch(error => {
+            console.log(error);
+          });
 
+        res.render("detail", { data: array });
+        console.log(myData);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+  //display the specific articles for a trending topic
+
+  //display the result of google cloud analysis of this topic.
 });
 
-
-
-
-router.get('/',function(req,res){
-const url = 'https://www.thecut.com/2019/03/an-awkward-kiss-changed-how-i-saw-joe-biden.html'
-axios.get(url)
-  .then(response => {
-    // console.log(response.data);
-    // console.log(cheerio('p', response.data).text());
-  })
-  .catch(error => {
-    console.log(error);
-  })
+router.get("/", function(req, res) {
+  const url =
+    "https://www.thecut.com/2019/03/an-awkward-kiss-changed-how-i-saw-joe-biden.html";
+  axios
+    .get(url)
+    .then(response => {
+      // console.log(response.data);
+      // console.log(cheerio('p', response.data).text());
+    })
+    .catch(error => {
+      console.log(error);
+    });
 
   // res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
-    // const axios = require('axios');
+  // const axios = require('axios');
   // const cheerio = require('cheerio');
 
-//   const app = document.getElementById('root')
-//   const container = document.createElement('div')
-//   container.setAttribute('class', 'container')
-//   app.appendChild(container)
+  //   const app = document.getElementById('root')
+  //   const container = document.createElement('div')
+  //   container.setAttribute('class', 'container')
+  //   app.appendChild(container)
 
-//   var request = new XMLHttpRequest()
+  //   var request = new XMLHttpRequest()
 
-    // <!-- <% data.forEach(function(data){ %> -->
-    // <!-- <% }) %> -->
-// request.open('GET', 'https://cors.io?https://us-central1-vision-migration.cloudfunctions.net/la_hacks_2019?market_code=0', true)
-let myData = null
-const apiUrl = 'https://us-central1-vision-migration.cloudfunctions.net/la_hacks_2019?market_code=1'
-axios.get(apiUrl)
-  .then(response => {
-    myData = response.data
-    console.log(response.data)
-    response.data.buckets[0].report.rollups.forEach(item => {
-      console.log(item.name);
+  // <!-- <% data.forEach(function(data){ %> -->
+  // <!-- <% }) %> -->
+  // request.open('GET', 'https://cors.io?https://us-central1-vision-migration.cloudfunctions.net/la_hacks_2019?market_code=0', true)
+  let myData = null;
+  const apiUrl =
+    "https://us-central1-vision-migration.cloudfunctions.net/la_hacks_2019?market_code=1";
+  axios
+    .get(apiUrl)
+    .then(response => {
+      myData = response.data;
+      console.log(response.data);
+      response.data.buckets[0].report.rollups.forEach(item => {
+        console.log(item.name);
+      });
+      res.render("index", { data: response.data.buckets[0].report.rollups });
+      console.log(myData);
     })
-  res.render('index', {data: response.data.buckets[0].report.rollups});
-  console.log(myData)
-  })
-  .catch(error => {
-    console.log(error);
-  })
+    .catch(error => {
+      console.log(error);
+    });
 
-//   const https = require('https');
-// request.onload = function () {
-//   var data = JSON.parse(this.response)
+  //   const https = require('https');
+  // request.onload = function () {
+  //   var data = JSON.parse(this.response)
 
-//   console.log('here')
-//   console.log(data.buckets[0].report.rollups[0].name)
+  //   console.log('here')
+  //   console.log(data.buckets[0].report.rollups[0].name)
 
-//   data.buckets[0].report.rollups.forEach(item => {
-//     console.log(item.name)
-//     const card = document.createElement('div')
-//     card.setAttribute('class','card')
-//     const h1 = document.createElement('h1')
-//     h1.textContent = item.name
-//     const a = document.createElement('a')
-//     var obj = item.top_articles_on_network[0]
-//     var keys = Object.keys(obj)
-//     var vals = Object.values(obj)
-//     a.textContent = vals
-//     a.href = keys
-//     container.appendChild(card)
-//     card.appendChild(h1)
-//     card.appendChild(a)
-//   })
-// }
+  //   data.buckets[0].report.rollups.forEach(item => {
+  //     console.log(item.name)
+  //     const card = document.createElement('div')
+  //     card.setAttribute('class','card')
+  //     const h1 = document.createElement('h1')
+  //     h1.textContent = item.name
+  //     const a = document.createElement('a')
+  //     var obj = item.top_articles_on_network[0]
+  //     var keys = Object.keys(obj)
+  //     var vals = Object.values(obj)
+  //     a.textContent = vals
+  //     a.href = keys
+  //     container.appendChild(card)
+  //     card.appendChild(h1)
+  //     card.appendChild(a)
+  //   })
+  // }
 
-// request.send()
+  // request.send()
 
-// var request2 = new XMLHttpRequest()
-// request2.open('GET','https://cors.io?https://www.thecut.com/2019/03/an-awkward-kiss-changed-how-i-saw-joe-biden.html',true)
-// request2.onload = function () {
-//   var doc = this.reponse
-//   console.log(doc)
-// }
-// request2.send()
+  // var request2 = new XMLHttpRequest()
+  // request2.open('GET','https://cors.io?https://www.thecut.com/2019/03/an-awkward-kiss-changed-how-i-saw-joe-biden.html',true)
+  // request2.onload = function () {
+  //   var doc = this.reponse
+  //   console.log(doc)
+  // }
+  // request2.send()
 
   // res.render('index');
-  if (myData != null)
-  console.log(myData);
+  if (myData != null) console.log(myData);
 });
 
 //add the router
-app.use('/', router);
+app.use("/", router);
 app.listen(process.env.port || 3000);
 
-console.log('Running at Port 3000');
-
-
-
+console.log("Running at Port 3000");
 
 //     <script type="text/javascript">
 //     // const axios = require('axios');
