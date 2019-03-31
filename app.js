2ad3ec98add48f6a4001f6202c823f901a440689
Shubham Kulkarni <shubhamkulkarni01@gmail.com>
2019-03-31 00:45:08 -0700

    the one true god

--- app.js
+++ app.js
@@ -188,33 +188,7 @@ function makeStuffCoherent(entity, entitysentiment) {
 }
 
 router.get("/:id?", function(req, res) {
-  const url =
-    "https://www.thecut.com/2019/03/an-awkward-kiss-changed-how-i-saw-joe-biden.html";
-  axios
-    .get(url)
-    .then(response => {
-      // console.log(response.data);
-      // console.log(cheerio('p', response.data).text());
-    })
-    .catch(error => {
-      console.log(error);
-    });
 
-  // res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-  // const axios = require('axios');
-  // const cheerio = require('cheerio');
-
-  //   const app = document.getElementById('root')
-  //   const container = document.createElement('div')
-  //   container.setAttribute('class', 'container')
-  //   app.appendChild(container)
-
-  //   var request = new XMLHttpRequest()
-
-  // <!-- <% data.forEach(function(data){ %> -->
-  // <!-- <% }) %> -->
-  // request.open('GET', 'https://cors.io?https://us-central1-vision-migration.cloudfunctions.net/la_hacks_2019?market_code=0', true)
   let myData = null;
   var locate = 0;
   let apiUrl =
@@ -242,42 +216,6 @@ router.get("/:id?", function(req, res) {
       console.log(error);
     });
 
-  //   const https = require('https');
-  // request.onload = function () {
-  //   var data = JSON.parse(this.response)
-
-  //   console.log('here')
-  //   console.log(data.buckets[0].report.rollups[0].name)
-
-  //   data.buckets[0].report.rollups.forEach(item => {
-  //     console.log(item.name)
-  //     const card = document.createElement('div')
-  //     card.setAttribute('class','card')
-  //     const h1 = document.createElement('h1')
-  //     h1.textContent = item.name
-  //     const a = document.createElement('a')
-  //     var obj = item.top_articles_on_network[0]
-  //     var keys = Object.keys(obj)
-  //     var vals = Object.values(obj)
-  //     a.textContent = vals
-  //     a.href = keys
-  //     container.appendChild(card)
-  //     card.appendChild(h1)
-  //     card.appendChild(a)
-  //   })
-  // }
-
-  // request.send()
-
-  // var request2 = new XMLHttpRequest()
-  // request2.open('GET','https://cors.io?https://www.thecut.com/2019/03/an-awkward-kiss-changed-how-i-saw-joe-biden.html',true)
-  // request2.onload = function () {
-  //   var doc = this.reponse
-  //   console.log(doc)
-  // }
-  // request2.send()
-
-  // res.render('index');
   if (myData != null) console.log(myData);
 });
 
@@ -287,50 +225,3 @@ app.listen(process.env.port || 3000);
 
 console.log("Running at Port 3000");
 
-//     <script type="text/javascript">
-//     // const axios = require('axios');
-//   // const cheerio = require('cheerio');
-
-//   const app = document.getElementById('root')
-//   const container = document.createElement('div')
-//   container.setAttribute('class', 'container')
-//   app.appendChild(container)
-
-//   var request = new XMLHttpRequest()
-// request.open('GET', 'https://cors.io?https://us-central1-vision-migration.cloudfunctions.net/la_hacks_2019?market_code=0', true)
-
-// request.onload = function () {
-//   var data = JSON.parse(this.response)
-
-//   console.log('here')
-//   console.log(data.buckets[0].report.rollups[0].name)
-
-//   data.buckets[0].report.rollups.forEach(item => {
-//     console.log(item.name)
-//     const card = document.createElement('div')
-//     card.setAttribute('class','card')
-//     const h1 = document.createElement('h1')
-//     h1.textContent = item.name
-//     const a = document.createElement('a')
-//     var obj = item.top_articles_on_network[0]
-//     var keys = Object.keys(obj)
-//     var vals = Object.values(obj)
-//     a.textContent = vals
-//     a.href = keys
-//     container.appendChild(card)
-//     card.appendChild(h1)
-//     card.appendChild(a)
-//   })
-// }
-
-// request.send()
-
-// // var request2 = new XMLHttpRequest()
-// // request2.open('GET','https://cors.io?https://www.thecut.com/2019/03/an-awkward-kiss-changed-how-i-saw-joe-biden.html',true)
-// // request2.onload = function () {
-// //   var doc = this.reponse
-// //   console.log(doc)
-// // }
-// // request2.send()
-
-//     </script>
